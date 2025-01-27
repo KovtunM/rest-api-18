6dd572a85afe0a0cb6129b10592f46b4115f4c02
Kovtun Mykhailo <kowtyn.m@gmail.com>
2025-01-27 21:45:49 +0200

    added specs

--- src/test/java/tests/ReqresInExtendedTests.java
+++ src/test/java/tests/ReqresInExtendedTests.java
@@ -6,6 +6,8 @@ import models.lombok.LoginResponseLombokModel;
 import models.pojo.LoginBodyPojoModel;
 import models.pojo.LoginResponsePojoModel;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static helpers.CustomAllureListener.withCustomTemplates;
 import static io.qameta.allure.Allure.step;
@@ -14,6 +16,8 @@ import static io.restassured.http.ContentType.JSON;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static specs.LoginSpec.loginRequestSpec;
+import static specs.LoginSpec.loginResponseSpec;
 
 public class ReqresInExtendedTests {
 
@@ -149,6 +153,52 @@ public class ReqresInExtendedTests {
                     .log().status()
                     .log().body()
                     .statusCode(200)
+                    .extract().as(LoginResponseLombokModel.class));
+
+        step("Verify response", () ->
+            assertThat(response.getToken()).isEqualTo("QpwL5tke4Pnpja7X4"));
+    }
+
+    @Test
+    void successfulLoginWithAllureSpecsTest() {
+
+        step("Prepare test data");
+
+        LoginBodyLombokModel loginBodyLombok = new LoginBodyLombokModel();
+        loginBodyLombok.setEmail("eve.holt@reqres.in");
+        loginBodyLombok.setPassword("cityslicka");
+
+        LoginResponseLombokModel response = step("Make request", () ->
+            given(loginRequestSpec)
+                    .body(loginBodyLombok)
+                    .when()
+                    .post()
+                    .then()
+                    .spec(loginResponseSpec)
+                    .extract().as(LoginResponseLombokModel.class));
+
+        step("Verify response", () ->
+            assertThat(response.getToken()).isEqualTo("QpwL5tke4Pnpja7X4"));
+    }
+
+
+    @ParameterizedTest()
+    @CsvSource(value = {"eve.holt@reqres.in:cityslicka"}, delimiter = ':')
+    void successfulLoginParametrizedTest(String login, String password) {
+
+        step("Prepare test data");
+
+        LoginBodyLombokModel loginBodyLombok = new LoginBodyLombokModel();
+        loginBodyLombok.setEmail(login);
+        loginBodyLombok.setPassword(password);
+
+        LoginResponseLombokModel response = step("Make request", () ->
+            given(loginRequestSpec)
+                    .body(loginBodyLombok)
+                    .when()
+                    .post()
+                    .then()
+                    .spec(loginResponseSpec)
                     .extract().as(LoginResponseLombokModel.class));
 
         step("Verify response", () ->
